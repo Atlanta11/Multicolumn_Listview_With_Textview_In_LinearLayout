3de44aebbb8e6114483a39fb20fb916ead383f0e
Enjoy <Enjoy>
2016-12-05 09:15:51 +0100

    Initial commit

---
+++ app/src/main/java/enterprise/sample/com/multicolumn_listview/Constants.java
@@ -0,0 +1,13 @@
+package enterprise.sample.com.multicolumn_listview;
+
+/**
+ * Created by ginov on 5/12/2016.
+ */
+
+public class Constants {
+
+    public static final String FIRST_COLUMN="First";
+    public static final String SECOND_COLUMN="Second";
+    public static final String THIRD_COLUMN="Third";
+    public static final String FOURTH_COLUMN="Fourth";
+}
